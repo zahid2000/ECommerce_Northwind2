81b007efecee8754160c6e33d0cf557753ca17ba
Zahid Mamedov <67357750+zahid2000@users.noreply.github.com>
2021-01-31 01:35:33 +0400

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+
+
+    public class EfProductDal : IProductDal
+    {
+
+        List<Product> _products;
+        public EfProductDal()
+        {
+            _products = new List<Product> { new Product {ProductName="Adapter" },new Product {ProductName="Su" } };
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir veritabani tablosudur
+    public interface IEntity
+    {
+    }
+}
